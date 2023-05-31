8126874b0ed77d94001896731980d8651bb6d96f
Lucile Grandjean <littlebip.lg@hotmail.fr>
2023-05-31 15:57:40 +0200

    Completed CR part of CRUD

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+puts "Cleaning the db..."
+Flat.destroy_all
+
+puts "Creating flats..."
+4.times do
+  flat = Flat.create!(
+    name: "#{Faker::Commerce.color.capitalize} & #{Faker::Commerce.color} Garden Flat #{Faker::Address.city}",
+    address: Faker::Address.full_address,
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: rand(75..150),
+    number_of_guests: rand(2..8)
+  )
+  puts "#{flat.name} created!"
+end
+
+puts "Seeding complete!"
